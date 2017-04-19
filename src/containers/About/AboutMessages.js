0b45a2d7256a3399a77b8a07cea4cb4ad48efb42
milos.kolcak <milos.kolcak@qest.cz>
2017-04-19 10:11:08 +0200

    Devstack React Redux example

---
+++ src/containers/About/AboutMessages.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Mikos on 18.04.2017.
+ */
+import {defineMessages} from 'react-intl';
+
+const messages: {
+  hello: Object,
+} = defineMessages({
+  hello: {
+    id: 'About.message.test',
+    description: 'Add translate description',
+    defaultMessage: 'Add default message'
+  }
+});
+
+export default messages;
